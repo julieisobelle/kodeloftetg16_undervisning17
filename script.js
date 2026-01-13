18a0c788542948d248467b7846c52553259fafc3
julieisobelle <julie.kodehode@gmail.com>
2026-01-13 12:12:13 +0100

    After lesson: fake login system with if/else

--- script.js
+++ script.js
@@ -32,10 +32,36 @@ const clock = 1010;
 
 if (clock === 930) {
   console.log("Oppmøte på Kodeloftet");
-} else if (clock >= 1000 && clock <= 1059) {
+} else if ((clock >= 1000 && clock <= 1059) || (clock >= 1111 && clock <= 1159)) {
   console.log("Undervisning pågår");
 } else if (clock === 1100 && clock <= 1110) {
   console.log("Pause i undervisning");
 } else {
   console.log("Undervisning er over");
 }
+
+//
+//
+
+const loginButton = document.querySelector("#loginButton");
+console.log(loginButton);
+
+loginButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // Manglet .value for å hente verdi og ikke element
+  const userEmail = document.querySelector("#userEmail").value;
+  console.log(userEmail);
+
+  // Manglet .value for å hente verdi og ikke element
+  const userPassword = document.querySelector("#userPassword").value;
+  console.log(userPassword);
+
+  // Manglet ! for å sjekke om noe IKKE finnes
+  if (!userEmail == "" || !userPassword == "") {
+    console.log("Data utfylt er: " + userEmail + " " + userPassword);
+  } else {
+    console.log("Something went wrong");
+    console.error("Something wrong:");
+  }
+});
